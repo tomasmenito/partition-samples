ca02db170849a31862a29249f893af76504a8083
tomaslm <tomaslm@hotmail.com>
2020-07-26 13:49:55 -0300

    Partition included on Purchase

--- partition_samples/bookstore/apps.py
+++ partition_samples/bookstore/apps.py
@@ -1,5 +1,29 @@
+from architect.commands import partition
 from django.apps import AppConfig
+from django.db import ProgrammingError
+from django.db.models.signals import post_migrate
+
+def create_partitions(sender, **kwargs):
+    """
+    After running migrations, go through each of the models
+    in the app and ensure the partitions have been setup
+    """
+    paths = {model.__module__ for model in sender.get_models()}
+    for path in paths:
+        try:
+            print(partition.run(dict(module=path)))
+        except ProgrammingError as e:
+            # Possibly because models were just un-migrated or
+            # fields have been changed that effect Architect
+            print(f"Unable to apply partitions for module '{path}'", e)
+        else:
+            print(f"Applied partitions for module '{path}'")
 
 
 class BookstoreConfig(AppConfig):
     name = 'bookstore'
+
+    def ready(self):
+        super(BookstoreConfig, self).ready()
+        # Hook up Architect to the post migrations signal
+        post_migrate.connect(create_partitions, sender=self)
--- partition_samples/bookstore/models.py
+++ partition_samples/bookstore/models.py
@@ -1,8 +1,16 @@
 import uuid
 
+import architect
 from django.db import models
 
+options = {
+    "type": "range",
+    "subtype": "date",
+    "constraint": "month",
+    "column": "purchased_at",    
+}
 
+@architect.install('partition', **options)
 class Purchase(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     book_id = models.UUIDField()
