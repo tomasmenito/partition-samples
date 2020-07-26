448762fe3cdbf9d59b62c53e640bca4cd71e7498
tomaslm <tomaslm@hotmail.com>
2020-07-26 11:43:50 -0300

    Creation Purchase model

--- partition_samples/bookstore/models.py
+++ partition_samples/bookstore/models.py
@@ -1,3 +1,10 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Purchase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    book_id = models.UUIDField()
+    buyer_id = models.UUIDField()
+    purchased_at = models.DateTimeField(null=False)
